4db6a88ed52e032af183fe051a6f8c983e9221ef
Reefaai <164148852+Reefaai@users.noreply.github.com>
2024-07-12 03:06:36 +0700

    Update twrp_X6812B.mk

--- twrp_X6812B.mk
+++ twrp_X6812B.mk
@@ -7,13 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from X6812B device
 $(call inherit-product, device/infinix/X6812B/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := X6812B
 PRODUCT_NAME := twrp_X6812B
